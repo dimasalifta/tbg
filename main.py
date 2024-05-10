e2613a7922b5bacdf166f77d8f30dbe5aaeb0902
sshrmn7889 <sshrmn7889@gmail.com>
2024-05-10 11:53:53 +0700

    add

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import sys
 from pysnmp.entity.rfc3413.oneliner import cmdgen
 import json
 import time
+import struct
+import minimalmodbus
 MQTT_BROKER = os.getenv("MQTT_BROKER")
 MQTT_TOPIC = os.getenv("MQTT_TOPIC")
 MQTT_PORT=os.getenv("MQTT_PORT")
@@ -101,14 +103,90 @@ list_volt = ["phase_l1_voltage",
              "dc_output_voltage",
              "battery_capacity_ah_1",
              "battery_capacity_ah_2"]
+# slave address (in decimal)
+DEVICE_ADDRESS_SHT20 = 1
+DEVICE_ADDRESS_ENERGY_METER = 2
 
-def convert_volt(x):
+# ENABLE/DISABLE communication debug mode
+DEVICE_DEBUG = False
+# Master PORT name -- Change as needed for your host.
+PORT_NAME = '/dev/ttyUSB0'
+
+# MODBUS instrument initialization
+sht20 = minimalmodbus.Instrument(PORT_NAME, DEVICE_ADDRESS_SHT20, debug=DEVICE_DEBUG)
+energy_meter = minimalmodbus.Instrument(PORT_NAME, DEVICE_ADDRESS_ENERGY_METER, debug=DEVICE_DEBUG)
+
+# MODBUS instrument connection settings
+# Change as needed depending on your Hardware requirements
+sht20.serial.baudrate = 9600
+sht20.serial.bytesize = 8
+sht20.serial.parity   = minimalmodbus.serial.PARITY_NONE
+sht20.serial.stopbits = 1
+sht20.mode = minimalmodbus.MODE_RTU
+sht20.serial.timeout = 1
+
+energy_meter.serial.baudrate = 9600
+energy_meter.serial.bytesize = 8
+energy_meter.serial.parity   = minimalmodbus.serial.PARITY_NONE
+energy_meter.serial.stopbits = 1
+energy_meter.mode = minimalmodbus.MODE_RTU
+energy_meter.serial.timeout = 1
+
+REGISTER_NUMBER_DECIMALS_SHT20 = 1
+REGISTER_NUMBER_DECIMALS_ENERGY_METER = 0
+ModBus_Command = 4
+
+list_register_sht20 = {
+    "temperature" : [1," Celcius"],
+    "humidity" : [2," %"] 
+}
+
+
+list_register_energy_meter = {
+    "l1_voltage" : [0," V"],
+    "l2_voltage" : [2," V"],
+    "l3_voltage" : [4," V"],
+
+    "total_ampere" : [6," A"],
+    "l1_ampere" : [8," A"],
+    "l2_ampere" : [10," A"],
+    "l3_ampere" : [12," A"],
+    
+    "total_power" : [16," kW"],
+    "l1_power" : [18," kW"],
+    "l2_power" : [20," kW"],
+    "l3_power" : [22," kW"],
+    
+    "total_kvarh" : [24," kVArh"],
+    "l1_kvarh" : [26," kVArh"],
+    "l2_kvarh" : [28," kVArh"],
+    "l3_kvarh" : [30," kVArh"],
+    
+    "l1_power_factor" : [42," theta"],
+    "l2_power_factor" : [44," theta"],
+    "l3_power_factor" : [46," theta"],
+    
+    "3phase_frequency" : [54," Hz"],
+    "energy_consumption" : [80," kWh"]
+    
+}
+def convert_decimal(x):
     angka = int(x)
     hasil_pembagian = angka / 1000
     # hasil_bulatan = round(hasil_pembagian)  
     hasil = str(hasil_pembagian)
     return hasil   
 
+def int_to_float(data):
+    # Convert 16-bit integer to bytes (2 bytes)
+    data_bytes = data.to_bytes(2, byteorder='big', signed=True)
+    # Extend the bytes to 4 bytes by adding 2 more bytes of zero
+    data_bytes += b'\x00\x00'
+    # Convert bytes to 32-bit float
+    float_value = struct.unpack('>f', data_bytes)[0]
+    # Bulatkan nilai float menjadi 3 angka di belakang koma
+    rounded_float_value = round(float_value, 3)
+    return rounded_float_value
 # def hitung_backup_time(kapasitas_baterai_Ah, beban_Watt):
 #     kapasitas_baterai = 100 # Ah
 #     beban = 500 # Watt
@@ -116,7 +194,28 @@ def convert_volt(x):
 #     backup_time_hours = kapasitas_baterai_Ah / konsumsi_daya_Watt
 #     return backup_time_hours
 
-
+def read_rs485():
+    try:
+        energy_meter_data = {}
+    # for key, values in list_register_sht20.items():
+    #     address = values[0]  # Ambil alamat register dari elemen pertama dalam daftar
+    #     unit = values[1]     # Ambil unit dari elemen kedua dalam daftar
+    #     value = sht20.read_register(address, REGISTER_NUMBER_DECIMALS_SHT20, ModBus_Command)
+    #     print(f"{key}: {value}{unit}")
+        
+        for key, values in list_register_energy_meter.items():
+            address = values[0]  # Ambil alamat register dari elemen pertama dalam daftar
+            unit = values[1]     # Ambil unit dari elemen kedua dalam daftar
+            value = energy_meter.read_register(address, REGISTER_NUMBER_DECIMALS_ENERGY_METER, ModBus_Command)
+            try:
+                value = int_to_float(value)
+                energy_meter_data[key] = value
+                # print(f"{key}: {value}{unit}")
+            except OverflowError:
+                print(f"{value}{unit} Nilai dari {key} terlalu besar untuk dikonversi menjadi float.")
+    except Exception as e:
+        print(f"Failed to read from instrument ------ {e}")
+    return energy_meter_data
 # Fungsi untuk menginisiFalisasi koneksi MQTT
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
@@ -160,7 +259,7 @@ def mqtt_process():
     client.connect(MQTT_BROKER, 1883, 60)
     client.loop_forever()
 
-def snmp_process(json_file):
+def snmp_process():
     while True:
         try:
             cmdGen = cmdgen.CommandGenerator()
@@ -187,11 +286,13 @@ def snmp_process(json_file):
                     # print(oid.prettyPrint(), val.prettyPrint())
                     # Menyimpan nilai ke dalam dictionary data
                     if param_name in list_volt:
-                        val = convert_volt(val.prettyPrint())
+                        val = convert_decimal(val.prettyPrint())
                         data[param_name] = val
                     else:
                         data[param_name] = val.prettyPrint()
 
+            print(read_rs485())
+            print(data)
             # Mengonversi data menjadi format JSON
             payload = json.dumps(data)
             
@@ -208,5 +309,5 @@ if __name__ == "__main__":
     # Buat process untuk menjalankan client MQTT
     mqtt_main = multiprocessing.Process(target=mqtt_process)
     mqtt_main.start()    
-    snmp_main = multiprocessing.Process(target=snmp_process('mc2600_oid_list.json')) 
+    snmp_main = multiprocessing.Process(target=snmp_process) 
     snmp_main.start() 
