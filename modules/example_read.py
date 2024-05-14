9c4bad43877c1bb2315de4932f5661ce1f7d320c
sshrmn7889 <sshrmn7889@gmail.com>
2024-05-14 23:59:19 +0700

    add

--- modules/example_read.py
+++ modules/example_read.py
@@ -5,8 +5,7 @@ import sht20
 temperature, humidity = sht20.read_sensor_data()
 
 # Display the values of the variables
-if temperature is not None and humidity is not None:
-    print(f"Temperature: {temperature} Celcius")
-    print(f"Humidity: {humidity} %")
-else:
-    print("Failed to retrieve sensor data")
+
+print(f"Temperature: {temperature} Celcius")
+print(f"Humidity: {humidity} %")
+
