4526530479b2ed081c1e4677357ad9dd86e06d8a
sshrmn7889 <sshrmn7889@gmail.com>
2024-05-15 02:14:38 +0700

    add

--- mainn.py
+++ mainn.py
@@ -2,4 +2,6 @@ from modules import rs485_sht20,rs485_energy,snmp_megmeet
 import time
 # Read sensor data
 data_sht20 = rs485_sht20.read_sensor_data(debug=True)
+time.sleep(1)
 data_energy = rs485_energy.read_sensor_data(debug=True)
+time.sleep(1)
