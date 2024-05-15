4c61d25e941dc2416d382b6c5942759e8436e195
sshrmn7889 <sshrmn7889@gmail.com>
2024-05-15 10:36:37 +0700

    add

--- modules/socket_ip.py
+++ modules/socket_ip.py
@@ -1,5 +1,10 @@
 import socket
-def read_sensor_data():
+def read_sensor_data(debug=False):
     hostname = socket.gethostname()
     local_ip = socket.gethostbyname(hostname)
+    if debug:
+        print("##################################################")
+        print(f"{__file__}")
+        print(local_ip)
+        print("##################################################")
     return local_ip
